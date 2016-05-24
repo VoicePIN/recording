2a0459f6d6210ea6ebc9a434c2fe2254f3a74e01
bartoszpawlowicz <pawlowicz.bartosz@gmail.com>
2016-05-23 18:05:00 -0700

    Update VPAudioManager.h

--- VPAudioManager.h
+++ VPAudioManager.h
@@ -3,7 +3,8 @@
 //  Voicepin Collector
 //
 //  Created by Marek Lipert on 26/04/15.
-//  Copyright (c) 2015 Voicepin. All rights reserved.
+//  Copyright (c) 2015 VoicePIN sp. z o.o.
+//  All rights reserved.
 //
 #import <Foundation/Foundation.h>
 @class VPAudioManager;
